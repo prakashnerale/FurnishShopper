ae474bbda38ae021fd54f24811c45ede4dbb8e4a
prakash <prakashnc@IMBGSL0090.kt.group.local>
2019-08-28 11:51:24 +0530

    1st commit

---
+++ FurnishShoppe/src/main/java/com/furnish/FurnishShoppeApplication.java
@@ -0,0 +1,13 @@
+package com.furnish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnishShoppeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnishShoppeApplication.class, args);
+	}
+
+}
