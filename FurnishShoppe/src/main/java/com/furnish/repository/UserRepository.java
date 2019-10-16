40c123c96eafbe5c1d52bc87715d33e8f34515d4
PrakashChandra <46310172+prakashnerale@users.noreply.github.com>
2019-10-16 12:49:04 +0530

    file deleted

--- FurnishShoppe/src/main/java/com/furnish/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.furnish.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.furnish.domain.User;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-	User findByUsername(String username);
-	User findByEmail(String email);
-}
