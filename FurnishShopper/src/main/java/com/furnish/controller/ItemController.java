5d27137d8365fd0707ea941c454e89e0676afb2d
PrakashChandra <46310172+prakashnerale@users.noreply.github.com>
2019-10-23 15:36:37 +0530

    Upload-image added

--- FurnishShopper/src/main/java/com/furnish/controller/ItemController.java
+++ FurnishShopper/src/main/java/com/furnish/controller/ItemController.java
@@ -1,12 +1,24 @@
 package com.furnish.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Iterator;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.furnish.domain.Item;
 import com.furnish.service.itemService;
@@ -15,14 +27,38 @@ import com.furnish.service.itemService;
 @RequestMapping("/item")
 @CrossOrigin(origins = "http://localhost:4200")
 public class ItemController {
-	
-  @Autowired
+
+	@Autowired
 	private itemService itemService;
-	
-	@RequestMapping(value="/add",method=RequestMethod.POST)
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	public Item addItemPost(@RequestBody Item item) {
 		return itemService.save(item);
 	}
-	
 
+	
+	@RequestMapping(value="/add/image", method=RequestMethod.POST)
+	public ResponseEntity upload(
+			@RequestParam("id") Long id,
+			HttpServletResponse response, HttpServletRequest request
+			){
+		try {
+			Item item = itemService.findById(id);
+			MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+			Iterator<String> it = multipartRequest.getFileNames();
+			MultipartFile multipartFile = multipartRequest.getFile(it.next());
+			String fileName = id+".png";
+			
+			
+			byte[] bytes = multipartFile.getBytes();
+			BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File("src/main/resources/static/image/item/"+fileName)));
+			stream.write(bytes);
+			stream.close();
+			
+			return new ResponseEntity("Upload Success!", HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity("Upload failed!", HttpStatus.BAD_REQUEST);
+		}
+	}
 }
